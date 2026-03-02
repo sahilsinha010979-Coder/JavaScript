c2f7ad9430352e90541e96086a721eec3c829fc8
Sahil Sinha <sahilsinha010979@gmail.com>
2026-03-02 22:03:32 +0530

    Add initial loop implementation in Loops.js

---
+++ Loops.js
@@ -0,0 +1,5 @@
+// Loops are used to execute a block of code repeatedly until a specified condition is met. There are several types of loops in JavaScript, including:
+// 1. For Loop
+for (let i = 0; i < 5; i++) {
+    console.log("For Loop Iteration: " , i);
+}
